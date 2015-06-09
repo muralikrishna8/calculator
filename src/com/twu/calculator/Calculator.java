c5902757729ebd4816b0354edc68e5deb40b0d36
muralikrishna8 <vmurali1444@gmail.com>
2015-06-09 14:13:15 +0530

    Arun/Murali - Pased test for executing add command

--- src/com/twu/calculator/Calculator.java
+++ src/com/twu/calculator/Calculator.java
@@ -1,5 +1,19 @@
 package com.twu.calculator;
 
-public class Calculator {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Calculator {
+    private double result;
+    private Map<String, Executable> commandActionMap;
+
+    public Calculator(AddCommand addCommand) {
+        commandActionMap = new HashMap<>();
+        commandActionMap.put("add", addCommand);
+    }
+
+    public void execute(String command, double operand) {
+        Executable executable = commandActionMap.get(command);
+        this.result = executable.execute();
+    }
 }
--- test/com/twu/calculator/CalculatorTest.java
+++ test/com/twu/calculator/CalculatorTest.java
@@ -1,9 +1,23 @@
 package com.twu.calculator;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class CalculatorTest {
+    @Mock
+    private AddCommand mockAddCommand;
 
+    private Calculator calculator;
+
+    @Test
+    public void shouldPerformAdditionOnAddCommand() {
+        calculator = new Calculator(mockAddCommand);
+        calculator.execute("add", 1.0);
+
+        Mockito.verify(mockAddCommand).execute();
+    }
 }
