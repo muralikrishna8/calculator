7e00ebf6bb6f2ddcf154a8c310c044f5765768d0
muralikrishna8 <vmurali1444@gmail.com>
2015-06-13 14:58:19 +0530

    Refactored moved typecasting and parsing to parser class

--- src/com/twu/calculator/Cancel.java
+++
@@ -1,9 +0,0 @@
-package com.twu.calculator;
-
-public class Cancel implements ArithmeticOperation {
-
-    @Override
-    public double execute(double firstOperand, double secondOperand) {
-        return 0;
-    }
-}
